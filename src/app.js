5f3e7cb07f19b5ff6db63f522204543e72befdc6
EugeneLobodaev <eugenelobodaev@gmail.com>
2023-08-27 19:31:41 +0300

    chore: code formated. fix: deleted console.log from index.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 import './scss/index.scss'
 import { updateTime } from '../src/scripts/services/clock.js'
-import { createTask } from './components/index.js'
-import tasks from '../tasksPlaceholdes.js'
 const main = () => {
-    updateTime()
-    console.log(createTask(tasks[0]))
+  updateTime()
 }
 export default main
--- src/components/Tasks/createCards.js
+++ src/components/Tasks/createCards.js
@@ -1,82 +1,81 @@
 import './styles.scss'
 export function createTask(task) {
-    const taskItem = document.createElement(`li`)
-    taskItem.classList.add(`tasks__list-item`)
-    taskItem.id = task.id
+  const taskItem = document.createElement(`li`)
+  taskItem.classList.add(`tasks__list-item`)
+  taskItem.id = task.id
 
-    const taskContainer = document.createElement(`section`)
-    taskContainer.className = `task`
+  const taskContainer = document.createElement(`article`)
+  taskContainer.className = `task`
 
-    const taskHeader = document.createElement(`header`)
-    taskHeader.className = `task__header`
+  const taskHeader = document.createElement(`header`)
+  taskHeader.className = `task__header`
 
-    const taskTitle = document.createElement(`h3`)
-    taskTitle.className = `task_title`
-    taskTitle.textContent = task.title
+  const taskTitle = document.createElement(`h3`)
+  taskTitle.className = `task_title`
+  taskTitle.textContent = task.title
 
-    const taskButtonsWrapper = document.createElement(`div`)
-    taskButtonsWrapper.className = `task__controls`
+  const taskButtonsWrapper = document.createElement(`div`)
+  taskButtonsWrapper.className = `task__controls`
 
-    const taskDescription = document.createElement(`p`)
-    taskDescription.className = `task__desctription`
-    taskDescription.textContent = task.description
+  const taskDescription = document.createElement(`p`)
+  taskDescription.className = `task__desctription`
+  taskDescription.textContent = task.description
 
-    const taskFooter = document.createElement(`footer`)
-    taskFooter.className = `task__footer`
+  const taskFooter = document.createElement(`footer`)
+  taskFooter.className = `task__footer`
 
-    const assignedUser = document.createElement(`p`)
-    assignedUser.className = `task__user`
-    assignedUser.textContent = task.user
+  const assignedUser = document.createElement(`p`)
+  assignedUser.className = `task__user`
+  assignedUser.textContent = task.user
 
-    const assignedDate = document.createElement(`p`)
-    assignedDate.className = `task__date`
-    assignedDate.textContent = task.date
+  const assignedDate = document.createElement(`p`)
+  assignedDate.className = `task__date`
+  assignedDate.textContent = task.date
 
-    if (task.stage === `todo`) {
-        const acceptButton = document.createElement(`button`)
-        acceptButton.type = `button`
-        acceptButton.className = `task__btn`
-        acceptButton.textContent = `Asign Task`
+  if (task.stage === `todo`) {
+    const acceptButton = document.createElement(`button`)
+    acceptButton.type = `button`
+    acceptButton.className = `task__btn`
+    acceptButton.textContent = `Asign Task`
 
-        const editButton = document.createElement(`button`)
-        editButton.type = `button`
-        editButton.className = `task__btn`
-        editButton.textContent = `Edit Task`
+    const editButton = document.createElement(`button`)
+    editButton.type = `button`
+    editButton.className = `task__btn`
+    editButton.textContent = `Edit Task`
 
-        const deleteButton = document.createElement(`button`)
-        deleteButton.type = `button`
-        deleteButton.className = `task__btn`
-        deleteButton.textContent = `Delete Task`
+    const deleteButton = document.createElement(`button`)
+    deleteButton.type = `button`
+    deleteButton.className = `task__btn`
+    deleteButton.textContent = `Delete Task`
 
-        taskButtonsWrapper.append(acceptButton, editButton, deleteButton)
+    taskButtonsWrapper.append(acceptButton, editButton, deleteButton)
+  }
+  if (task.stage === `inprogress`) {
+    const backButton = document.createElement(`button`)
+    backButton.type = `button`
+    backButton.className = `task__btn`
+    backButton.textContent = `Unassign`
 
-    }
-    if (task.stage === `inprogress`) {
-        const backButton = document.createElement(`button`)
-        backButton.type = `button`
-        backButton.className = `task__btn`
-        backButton.textContent = `Unassign`
+    const completeTask = document.createElement(`button`)
+    completeTask.type = `button`
+    completeTask.className = `task__btn`
+    completeTask.textContent = `Completed!`
 
-        const completeTask = document.createElement(`button`)
-        completeTask.type = `button`
-        completeTask.className = `task__btn`
-        completeTask.textContent = `Completed!`
+    taskButtonsWrapper.append(backButton, completeTask)
+  }
 
-        taskButtonsWrapper.append(backButton, completeTask)
-    }
+  if (task.stage === `done`) {
+    const deleteButton = document.createElement(`button`)
+    deleteButton.type = `button`
+    deleteButton.className = `task__btn`
+    deleteButton.textContent = `Clear`
 
-    if (task.stage === `done`) {
-        const deleteButton = document.createElement(`button`)
-        deleteButton.type = `button`
-        deleteButton.className = `task__btn`
-        deleteButton.textContent = `Clear`
+    taskButtonsWrapper.append(deleteButton)
+  }
 
-        taskButtonsWrapper.append(deleteButton)
-    }
-
-    taskHeader.append(taskTitle, taskButtonsWrapper)
-    taskFooter.append(assignedUser, assignedDate)
-    taskContainer.append(taskHeader, taskDescription, taskFooter)
-    taskItem.append(taskContainer)
-    return taskItem
+  taskHeader.append(taskTitle, taskButtonsWrapper)
+  taskFooter.append(assignedUser, assignedDate)
+  taskContainer.append(taskHeader, taskDescription, taskFooter)
+  taskItem.append(taskContainer)
+  return taskItem
 }
