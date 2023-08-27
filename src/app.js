5bcce97c0926c8cd1118d25b07647176c01b896a
EugeneLobodaev <eugenelobodaev@gmail.com>
2023-08-27 19:05:32 +0300

    feat: create card func added. styles added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import './scss/index.scss'
 import { updateTime } from '../src/scripts/services/clock.js'
-updateTime()
-const main = () => {}
+import { createTask } from './components/index.js'
+import tasks from '../tasksPlaceholdes.js'
+const main = () => {
+    updateTime()
+    console.log(createTask(tasks[0]))
+}
 export default main
