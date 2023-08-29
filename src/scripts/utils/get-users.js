28188b94fe1c2fd0d06436a94eaccd73971f000d
ravenwol1 <olegmurashkoo@gmail.com>
2023-08-29 23:53:32 +0300

    patch getusernames function

---
+++ src/scripts/utils/get-users.js
@@ -0,0 +1,12 @@
+export async function getUserNames() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const usersData = await response.json()
+
+    return usersData.map(user => user.name)
+  } catch (error) {
+    console.error(`Error: ${error}`)
+    return []
+  }
+}
+await getUserNames()
