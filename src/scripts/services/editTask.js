03cad7b0515aef7665ec05fdd0a5755f99d23dad
EugeneLobodaev <eugenelobodaev@gmail.com>
2023-08-31 12:53:05 +0300

    fix edit

--- src/scripts/services/editTask.js
+++ src/scripts/services/editTask.js
@@ -2,4 +2,14 @@ import { showModal } from './modals.js'
 
 export const editTask = () => {
   showModal()
+  const modalButtonSubmit = document.querySelector(`.modal__button`)
+  modalButtonSubmit.addEventListener(`submit`, event => {
+    // const taskHeader = document.querySelector(`.task__header`)
+    // const taskDescription = document.querySelector(`.task__description`)
+    // const taskFooter = document.querySelector(`.task__footer`)
+    // taskHeader = Инпуту хедера
+    // taskDescription = инпуту дескрипшена
+    // taskFooter = инпуту футера
+    // renderTasks()
+  })
 }
--- src/scripts/services/modals.js
+++ src/scripts/services/modals.js
@@ -10,15 +10,4 @@ modalButtonClose.addEventListener(`click`, () => {
   showModal()
 })
 
-const modalButtonSubmit = document.querySelector(`.modal__button`)
-modalButtonSubmit.addEventListener(`submit`, event => {
-  // const taskHeader = document.querySelector(`.task__header`)
-  // const taskDescription = document.querySelector(`.task__description`)
-  // const taskFooter = document.querySelector(`.task__footer`)
-  // taskHeader = Инпуту хедера
-  // taskDescription = инпуту дескрипшена
-  // taskFooter = инпуту футера
-  // renderTasks()
-})
-
 export const submitChanges = () => {}
