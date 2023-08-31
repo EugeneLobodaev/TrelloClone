02ed42850ef5ce5c6cb15ef950a9e9ad8c06ff9e
Zhdanovich <Zhiv80@mail.ru>
2023-08-31 23:13:56 +0300

    fixed\updateCounters

--- src/scripts/services/updateCounters.js
+++ src/scripts/services/updateCounters.js
@@ -2,8 +2,8 @@ export function updateCounters(tasks) {
   const tasksToDo = tasks.filter(task => task.stage === 'todo')
   const tasksInProgress = tasks.filter(task => task.stage === 'inprogress')
   const tasksDone = tasks.filter(task => task.stage === 'done')
-  document.getElementById('todo-items-count').textConent = tasksToDo
+  document.getElementById('todo-items-count').textConent = tasksToDo.length
   document.getElementById('in-progress-items-count').textConent =
-    tasksInProgress
-  document.getElementById('done-items-count').textConent = tasksDone
+    tasksInProgress.length
+  document.getElementById('done-items-count').textConent = tasksDone.length
 }
